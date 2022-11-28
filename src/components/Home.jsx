e864e7a81396378cb586968fe533023f7559a896
DipSapkota125 <dipsapkota325@gmail.com>
2022-11-28 16:42:29 +0545

    Add expression

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import wakanda from "../images/wakanda.png";
+import "./Home.css";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <h1 className="heading">Wakanda Forever</h1>
+      <div className="img_div">
+        <img src={wakanda} alt="wakanda" />
+        <img src={wakanda} alt="wakanda" />
+        <img src={wakanda} alt="wakanda" />
+      </div>
+    </>
+  );
 };
 
 export default Home;
